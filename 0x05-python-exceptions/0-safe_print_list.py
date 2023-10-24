7bc99c1800c4b280e0c6786778497854ad1de8eb
jedidiah <jedidahfavour@gmail.com>
2023-10-25 02:04:17 +0300

    fix errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
 
-    """A function that prints x elements of a list"""
-    
-    try:
-        count = 0
-        for i in my_list:
-            if count < x:
-                print(i, end=" ")
-                count += 1
-            print()
-            return count
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return count
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return (count)
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """a function that prints an integer with "{:d}".format()""" 
+    """a function that prints an integer with "{:d}".format()"""
     try:
         print("{:d}".format(value))
         return (True)
